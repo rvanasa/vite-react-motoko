186de3c98bfc073d5ea84e6d5e295ab5419ec99d
rvanasa <ryan.vandersmith@dfinity.org>
2023-03-05 11:47:41 -0700

    Fix Vite proxy on recent Node.js versions

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:4943',
+                target: 'http://127.0.0.1:4943',
                 // changeOrigin: true,
             },
         },
