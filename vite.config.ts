f2e7bf8e48bcee9e3fbd1623573e781e39695dd9
rvanasa <ryan.vandersmith@dfinity.org>
2023-04-03 10:24:51 -0600

    Detect non-local canister IDs from top-level 'canister_ids.json' file

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,45 @@ import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 import { defineConfig } from 'vite';
 
-const canisterIdPath = join(__dirname, '.dfx/local/canister_ids.json');
-if (!existsSync(canisterIdPath)) {
-    throw new Error(
-        'Unable to find canisters. Running `dfx deploy` should fix this problem.',
-    );
-}
-const canisterIds = JSON.parse(readFileSync(canisterIdPath, 'utf8'));
-
 const defaultNetwork = 'local';
 const network = process.env['DFX_NETWORK'] || defaultNetwork;
 
+let canisterIdPath: string;
+if (network == 'local') {
+  // Local replica canister IDs
+  canisterIdPath = join(__dirname, '.dfx/local/canister_ids.json');
+} else {
+  // Custom canister IDs
+  canisterIdPath = join(__dirname, 'canister_ids.json');
+}
+
+if (!existsSync(canisterIdPath)) {
+  throw new Error(
+    'Unable to find canisters. Running `dfx deploy` should fix this problem.',
+  );
+}
+const canisterIds = JSON.parse(readFileSync(canisterIdPath, 'utf8'));
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        global: 'window',
-        'process.env.DFX_NETWORK': JSON.stringify(process.env.DFX_NETWORK),
-        // Expose canister IDs provided by `dfx deploy`
-        ...Object.fromEntries(
-            Object.entries(canisterIds).map(([name, ids]) => [
-                `process.env.${name.toUpperCase()}_CANISTER_ID`,
-                JSON.stringify(ids[network] || ids[defaultNetwork]),
-            ]),
-        ),
+  plugins: [react()],
+  define: {
+    global: 'window',
+    'process.env.DFX_NETWORK': JSON.stringify(process.env.DFX_NETWORK),
+    // Expose canister IDs provided by `dfx deploy`
+    ...Object.fromEntries(
+      Object.entries(canisterIds).map(([name, ids]) => [
+        `process.env.${name.toUpperCase()}_CANISTER_ID`,
+        JSON.stringify(ids[network] || ids[defaultNetwork]),
+      ]),
+    ),
+  },
+  server: {
+    // Local IC replica proxy
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4943',
+      },
     },
-    server: {
-        // Local IC replica proxy
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:4943',
-            },
-        },
-    },
+  },
 });
