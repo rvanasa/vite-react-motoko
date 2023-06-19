2a3aa1af3f8597798faa29f22b6a25367135fde4
rvanasa <ryan.vandersmith@dfinity.org>
2023-06-19 11:55:52 -0600

    Simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { existsSync, readFileSync } from 'fs';
-import { join } from 'path';
 import { defineConfig } from 'vite';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
 
-const localNetwork = 'local';
-const network = process.env['DFX_NETWORK'] || localNetwork;
+dotenv.config();
 
-let canisterIdPath;
-if (network === localNetwork) {
-  // Local replica canister IDs
-  canisterIdPath = join(__dirname, '.dfx/local/canister_ids.json');
-} else {
-  // Custom canister IDs
-  canisterIdPath = join(__dirname, 'canister_ids.json');
-}
-
-if (!existsSync(canisterIdPath)) {
-  throw new Error(
-    'Unable to find canisters. Running `dfx deploy` should fix this problem.',
-  );
-}
-const canisterIds = JSON.parse(readFileSync(canisterIdPath, 'utf8'));
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.DFX_NETWORK': JSON.stringify(process.env.DFX_NETWORK),
-    // Expose canister IDs provided by `dfx deploy`
-    ...Object.fromEntries(
-      Object.entries(canisterIds).map(([name, ids]) => [
-        `process.env.${name.toUpperCase()}_CANISTER_ID`,
-        JSON.stringify(ids[network] || ids[localNetwork]),
-      ]),
-    ),
+  root: 'src',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
   optimizeDeps: {
     esbuildOptions: {
@@ -43,11 +19,17 @@ export default defineConfig({
     },
   },
   server: {
-    // Local IC replica proxy
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:4943',
+        changeOrigin: true,
       },
     },
   },
+  plugins: [
+    react(),
+    environment('all', { prefix: 'CANISTER_' }),
+    environment('all', { prefix: 'DFX_' }),
+    environment({ BACKEND_CANISTER_ID: '' }),
+  ],
 });
