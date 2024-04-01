7738e19f4f34e7737e0d13d3b83826037d3d99d8
b3hr4d <behradmusiclove@yahoo.com>
2024-04-01 21:54:44 +0300

    Add node compatibility flag and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,19 @@
-import { useEffect, useState } from 'react';
-import motokoLogo from './assets/motoko_moving.png';
-import motokoShadowLogo from './assets/motoko_shadow.png';
-import reactLogo from './assets/react.svg';
-import { backend } from './declarations/backend';
+import motokoLogo from "./assets/motoko_moving.png"
+import motokoShadowLogo from "./assets/motoko_shadow.png"
+import reactLogo from "./assets/react.svg"
+import { useQueryCall, useUpdateCall } from "@ic-reactor/react"
 
 function App() {
-  const [count, setCount] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data: count, call: refetchCount } = useQueryCall({
+    functionName: "get",
+  })
 
-  // Get the current counter value
-  const fetchCount = async () => {
-    try {
-      setLoading(true);
-      const count = await backend.get();
-      setCount(+count.toString()); // Convert BigInt to number
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const increment = async () => {
-    if (loading) return; // Cancel if waiting for a new count
-    try {
-      setLoading(true);
-      await backend.inc(); // Increment the count by 1
-      await fetchCount(); // Fetch the new count
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Fetch the count on page load
-  useEffect(() => {
-    fetchCount();
-  }, []);
+  const { call: increment, loading } = useUpdateCall({
+    functionName: "inc",
+    onSuccess: () => {
+      refetchCount()
+    },
+  })
 
   return (
     <div className="App">
@@ -62,8 +40,8 @@ function App() {
       </div>
       <h1>Vite + React + Motoko</h1>
       <div className="card">
-        <button onClick={increment} style={{ opacity: loading ? 0.5 : 1 }}>
-          count is {count}
+        <button onClick={increment} disabled={loading}>
+          count is {count?.toString() ?? "loading..."}
         </button>
         <p>
           Edit <code>backend/Backend.mo</code> and save to test HMR
@@ -73,7 +51,7 @@ function App() {
         Click on the Vite, React, and Motoko logos to learn more
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { ActorProvider, AgentProvider } from "@ic-reactor/react"
+import { idlFactory, canisterId } from "./declarations/backend"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <AgentProvider withProcessEnv>
+      <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
+        <App />
+      </ActorProvider>
+    </AgentProvider>
+  </React.StrictMode>
+)
