4615160028bbef9a844f6e213c68eb8a5434014b
b3hr4d <behradmusiclove@yahoo.com>
2024-11-30 15:23:34 +0300

    Add .ic-assets.json5, refactor imports, and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
+import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import './App.css';
 import motokoLogo from './assets/motoko_moving.png';
 import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
-import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 
 function App() {
-  const { data: count, call: refetchCount } = useQueryCall({
+  const { data: count, refetch } = useQueryCall({
     functionName: 'get',
   });
 
   const { call: increment, loading } = useUpdateCall({
     functionName: 'inc',
-    onSuccess: () => {
-      refetchCount();
-    },
+    onSuccess: refetch,
   });
 
   return (
